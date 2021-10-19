1c37d095abd20e492edd76940a81dc3b9e58939f
Caleb Woods <woodscaleb@gmail.com>
2021-10-19 10:00:27 -0700

    added register button

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
   # GET /events or /events.json
   def index
     if params[:public] == 'false'
-      @events = Event.where("event_details ->> 'image' != ''")
+      @events = Event
                      .order(Arel.sql "(event_details ->> 'start_datetime')::date  ASC")
     elsif params[:past] == 'true'
     @events = Event.where("event_details ->> 'public_calendar_listed' = 'true'" )
