10232021c454c7c1bf29ef4c46d25b1778749212
Caleb Woods <woodscaleb@gmail.com>
2021-10-20 13:12:24 -0700

    added optional date selection to backfill

--- app/jobs/update_events_job.rb
+++ app/jobs/update_events_job.rb
@@ -16,7 +16,7 @@ class UpdateEventsJob < ApplicationJob
                     :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
 
       request = Net::HTTP::Get.new uri.request_uri
-      request.basic_auth "JoshFelise",  "Jpf859675!"
+      request.basic_auth ENV['USERNAME'],  ENV['PASSWORD']
 
       response = http.request request # Net::HTTPResponse object
 
