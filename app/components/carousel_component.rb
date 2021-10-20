879094317f2c5cfdf2e0cdcfd95bd0b00b974948
Caleb Woods <woodscaleb@gmail.com>
2021-10-20 12:40:35 -0700

    added recurring events to slideshow

--- app/components/carousel_component.rb
+++ app/components/carousel_component.rb
@@ -2,20 +2,22 @@
 
 class CarouselComponent < ViewComponent::Base
   def initialize(events:)
-
     @events = []
-
     events.each do |event|
       if event.event_details['resources'].nil?
       else
         if event.event_details['resources']['resource'].class == Hash
           if event.event_details['resources']['resource']['name'] == 'Featured Event' && event.event_details['resources']['resource']['status'] == 'Approved'
             @events.append(event)
+          elsif event.event_details['resources']['resource']['name'] == 'Reoccurring Events'
+            @events.append(event)
           else
           end
         else
           event.event_details['resources']['resource'].each do |e|
             if e['name'] == 'Featured Event' && e['status'] == 'Approved'
+              @events.append(event)
+            elsif e['name'] == 'Reoccurring Events'
               @events.append(event)
             end
           end
--- app/components/list_component.rb
+++ app/components/list_component.rb
@@ -2,26 +2,18 @@
 
 class ListComponent < ViewComponent::Base
   def initialize(events:)
-
     @events = []
-
     events.each do |event|
-
       if event.event_details['resources'].nil?
-
       else
-
         if event.event_details['resources']['resource'].class == Hash
-
           if event.event_details['resources']['resource']['name'] == 'Featured Event' && event.event_details['resources']['resource']['status'] == 'Approved'
             @events.append(event)
           elsif event.event_details['resources']['resource']['name'] == 'Reoccurring Events'
             @events.append(event)
           else
           end
-
         else
-
           event.event_details['resources']['resource'].each do |e|
             if e['name'] == 'Featured Event' && e['status'] == 'Approved'
               @events.append(event)
@@ -29,9 +21,7 @@ class ListComponent < ViewComponent::Base
               @events.append(event)
             end
           end
-
         end
-
       end
     end
   end
